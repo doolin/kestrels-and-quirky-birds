93c59746c60b0f4da801cef39cdbe716227f3ca9
David Doolin <david.doolin@gmail.com>
2017-04-30 06:17:36 -0700

    start of elixir combinators

---
+++ elixir/lib/kestrels.ex
@@ -0,0 +1,18 @@
+defmodule Kestrels do
+  @moduledoc """
+  Documentation for Kestrels.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kestrels.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/test/kestrels_test.exs
@@ -0,0 +1,8 @@
+defmodule KestrelsTest do
+  use ExUnit.Case
+  doctest Kestrels
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
